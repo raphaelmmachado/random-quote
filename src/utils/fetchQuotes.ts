062f5195675bccaa505f83a33e37ca5ccaf4c3be
raphaelmmachado <raphael.mm.91@gmail.com>
2023-05-11 02:19:27 -0300

    2 files modified

--- src/utils/fetchQuotes.ts
+++ src/utils/fetchQuotes.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
-const key = import.meta.env.VITE_API_KEY;
-const fetchQuotes = async (category = "history") => {
-  const url = "https://api.api-ninjas.com/v1/quotes?category=";
+import axios from 'axios';
+const key = 'XtSD6nb+1I40bL6kGusGKg==fgpLbu1ahLyk7ohg';
+const fetchQuotes = async (category = 'history') => {
+  const url = 'https://api.api-ninjas.com/v1/quotes?category=';
   const res = await axios.get(`${url}${category}`, {
-    headers: { "X-Api-Key": key, "Content-Type": "application/json" },
+    headers: { 'X-Api-Key': key, 'Content-Type': 'application/json' },
   });
   return res;
 };
